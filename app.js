9f3eed0f247a343e52b0cdc7503fd5a44f7d2457
sironekotoro <develop@sironekotoro.com>
2017-12-24 02:13:03 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 'use strict';
 
+// アルゴリズム改善のためメモ化する
+// n番目,n番目のフィボナッチ数
+// という連想配列を作る
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
 
 // n番目のフィボナッチ数を求める
 // フィボナッチ数列
 // 0,1,1,2,3,5,8,13,21,34,55...
 function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
+    // has:連想配列memoにnという値があればtrue
+    if (memo.has(n)) {
+        return memo.get(n);
     }
-    return fib(n - 1) + fib(n - 2);
+    let value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
 }
 
 const length = 40;
